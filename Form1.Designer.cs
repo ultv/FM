7c5ef7456f0fb790fcef415e0fe45fa6244b31c2
Alex <ultv@inbox.ru>
2018-06-26 01:18:57 +0400

    Yandex Music test

---
+++ Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace FM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(367, 368);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(75, 23);
+            this.buttonSearch.TabIndex = 0;
+            this.buttonSearch.Text = "Искать";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(50, 41);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(496, 286);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.LinkClicked += new System.Windows.Forms.LinkClickedEventHandler(this.richTextBox1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.buttonSearch);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+
+namespace FM
+{
+    public partial class Form1 : Form
+    {
+        static IWebDriver browser;
+        readonly string url = "https://music.yandex.ru/genres";
+        //By LinkGenres = By.LinkText("Жанры");
+
+        By LinkRun = By.LinkText("Бег");
+        By LinkAllTrack = By.LinkText("Все треки");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            OpenBrowser(url);            
+
+            ((IJavaScriptExecutor)browser).ExecuteScript("arguments[0].scrollIntoView();", browser.FindElement(LinkRun));
+            browser.FindElement(LinkRun).Click();
+
+            ((IJavaScriptExecutor)browser).ExecuteScript("arguments[0].scrollIntoView();", browser.FindElement(LinkAllTrack));
+            browser.FindElement(LinkAllTrack).Click();
+
+            List<IWebElement> catalog = browser.FindElements(By.CssSelector(".d-track__name a")).ToList();
+
+            foreach(IWebElement element in catalog)
+            {
+                richTextBox1.AppendText(element.Text + "\n");
+                richTextBox1.AppendText(element.GetAttribute("href") + "\n");
+                richTextBox1.AppendText("\n");
+            }
+        }
+
+        /// <summary>
+        /// Открытие браузера.
+        /// </summary>
+        /// <param name="url">Адрес открываемой страницы</param>
+        public void OpenBrowser(string url)
+        {
+            if (browser == null)
+            {
+                browser = new OpenQA.Selenium.Chrome.ChromeDriver();
+                browser.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
+            }
+
+            browser.Manage().Window.Maximize();
+            browser.Navigate().GoToUrl(url);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (browser != null)
+            {
+                browser.Quit();
+            }
+        }
+
+        private void richTextBox1_LinkClicked(object sender, LinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(e.LinkText);
+        }
+    }
+}
