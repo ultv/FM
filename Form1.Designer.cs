901b0cb0a4e8b727104201c7d358e1bb59483e48
Alex <ultv@inbox.ru>
2018-06-26 17:31:42 +0400

    Create MuzoFon

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,12 +29,15 @@
         private void InitializeComponent()
         {
             this.buttonSearch = new System.Windows.Forms.Button();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBoxCatalogTrack = new System.Windows.Forms.RichTextBox();
+            this.richTextBoxTrackDuration = new System.Windows.Forms.RichTextBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.buttonDownload = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // buttonSearch
             // 
-            this.buttonSearch.Location = new System.Drawing.Point(367, 368);
+            this.buttonSearch.Location = new System.Drawing.Point(360, 403);
             this.buttonSearch.Name = "buttonSearch";
             this.buttonSearch.Size = new System.Drawing.Size(75, 23);
             this.buttonSearch.TabIndex = 0;
@@ -42,33 +45,69 @@
             this.buttonSearch.UseVisualStyleBackColor = true;
             this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
             // 
-            // richTextBox1
+            // richTextBoxCatalogTrack
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(50, 41);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(496, 286);
-            this.richTextBox1.TabIndex = 1;
-            this.richTextBox1.Text = "";
-            this.richTextBox1.LinkClicked += new System.Windows.Forms.LinkClickedEventHandler(this.richTextBox1_LinkClicked);
+            this.richTextBoxCatalogTrack.Location = new System.Drawing.Point(50, 41);
+            this.richTextBoxCatalogTrack.Name = "richTextBoxCatalogTrack";
+            this.richTextBoxCatalogTrack.Size = new System.Drawing.Size(405, 286);
+            this.richTextBoxCatalogTrack.TabIndex = 1;
+            this.richTextBoxCatalogTrack.Text = "";
+            this.richTextBoxCatalogTrack.LinkClicked += new System.Windows.Forms.LinkClickedEventHandler(this.richTextBox1_LinkClicked);
+            // 
+            // richTextBoxTrackDuration
+            // 
+            this.richTextBoxTrackDuration.Location = new System.Drawing.Point(487, 41);
+            this.richTextBoxTrackDuration.Name = "richTextBoxTrackDuration";
+            this.richTextBoxTrackDuration.Size = new System.Drawing.Size(94, 286);
+            this.richTextBoxTrackDuration.TabIndex = 2;
+            this.richTextBoxTrackDuration.Text = "";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(50, 356);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(55, 13);
+            this.linkLabel1.TabIndex = 3;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "linkLabel1";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // buttonDownload
+            // 
+            this.buttonDownload.Location = new System.Drawing.Point(125, 351);
+            this.buttonDownload.Name = "buttonDownload";
+            this.buttonDownload.Size = new System.Drawing.Size(75, 23);
+            this.buttonDownload.TabIndex = 4;
+            this.buttonDownload.Text = "Сохранить";
+            this.buttonDownload.UseVisualStyleBackColor = true;
+            this.buttonDownload.Click += new System.EventHandler(this.buttonDownload_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.buttonDownload);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.richTextBoxTrackDuration);
+            this.Controls.Add(this.richTextBoxCatalogTrack);
             this.Controls.Add(this.buttonSearch);
             this.Name = "Form1";
             this.Text = "Form1";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button buttonSearch;
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBoxCatalogTrack;
+        private System.Windows.Forms.RichTextBox richTextBoxTrackDuration;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Button buttonDownload;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+
+
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,17 +10,34 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
 
 namespace FM
 {
     public partial class Form1 : Form
     {
         static IWebDriver browser;
-        readonly string url = "https://music.yandex.ru/genres";
-        //By LinkGenres = By.LinkText("Жанры");
 
+        /// <summary>
+        /// ЯндексМузыка.
+        /// </summary>
+        //readonly string url = "https://music.yandex.ru/genres";                
         By LinkRun = By.LinkText("Бег");
         By LinkAllTrack = By.LinkText("Все треки");
+        By LinkTrackName = By.CssSelector(".d-track__name a");
+        By TxtTrackDuration = By.CssSelector(".d-track__info span");
+
+        /// <summary>
+        /// Музофон.
+        /// </summary>
+        readonly string url = "https://muzofond.org/";
+        By LinkSportMusic = By.LinkText("Спортивная музыка");
+        By LinkMeditationMusic = By.LinkText("Музыка Для Медитации");        
+        By IconDownload = By.CssSelector("div.actions > ul > li.download > a");
+        By TxtArtist = By.CssSelector("div.desc > h3 > span.artist");
+        By IconPlay = By.CssSelector("div.actions > ul > li.play");
+
+        
 
         public Form1()
         {
@@ -27,22 +46,64 @@ namespace FM
 
         private void buttonSearch_Click(object sender, EventArgs e)
         {
-            OpenBrowser(url);            
+            OpenBrowser(url);
 
+            //SelectYandexSound();
+            SelectMuzoFon();
+            
+            
+            //this.Activate();
+            //browser.Manage().Window.Minimize();
+        }
+
+        /// <summary>
+        /// Работа с сайтом ЯндексМузыка.
+        /// </summary>
+        private void SelectYandexSound()
+        {
             ((IJavaScriptExecutor)browser).ExecuteScript("arguments[0].scrollIntoView();", browser.FindElement(LinkRun));
             browser.FindElement(LinkRun).Click();
 
             ((IJavaScriptExecutor)browser).ExecuteScript("arguments[0].scrollIntoView();", browser.FindElement(LinkAllTrack));
             browser.FindElement(LinkAllTrack).Click();
 
-            List<IWebElement> catalog = browser.FindElements(By.CssSelector(".d-track__name a")).ToList();
+            //WebDriverWait browserWait = new WebDriverWait(browser, TimeSpan.FromSeconds(15));
+            //IWebElement LinkTrackNameWait = browserWait.Until(ExpectedConditions.ElementIsVisible(LinkTrackName));
 
-            foreach(IWebElement element in catalog)
+            List<IWebElement> catalogTrack = browser.FindElements(LinkTrackName).ToList();
+            List<IWebElement> catalogTrackDuration = browser.FindElements(TxtTrackDuration).ToList();
+
+            if (catalogTrack.Count > 0)
             {
-                richTextBox1.AppendText(element.Text + "\n");
-                richTextBox1.AppendText(element.GetAttribute("href") + "\n");
-                richTextBox1.AppendText("\n");
+                foreach (IWebElement element in catalogTrack)
+                {
+                    richTextBoxCatalogTrack.AppendText(element.Text + "\n");
+                    richTextBoxCatalogTrack.AppendText(element.GetAttribute("href") + "\n");
+                    richTextBoxCatalogTrack.AppendText("\n");
+                }
             }
+
+            if (catalogTrackDuration.Count > 0)
+            {
+                foreach (IWebElement element in catalogTrackDuration)
+                {
+                    richTextBoxTrackDuration.AppendText(element.Text + "\n");
+                    richTextBoxTrackDuration.AppendText("\n");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Работа с сайтом МузоФон.
+        /// </summary>
+        private void SelectMuzoFon()
+        {
+            ((IJavaScriptExecutor)browser).ExecuteScript("arguments[0].scrollIntoView();", browser.FindElement(LinkSportMusic));
+
+            browser.FindElement(LinkMeditationMusic).Click();
+
+            richTextBoxCatalogTrack.AppendText(browser.FindElement(IconDownload).GetAttribute("href"));
+            linkLabel1.Text = browser.FindElement(TxtArtist).Text;
         }
 
         /// <summary>
@@ -72,6 +133,16 @@ namespace FM
         private void richTextBox1_LinkClicked(object sender, LinkClickedEventArgs e)
         {
             System.Diagnostics.Process.Start(e.LinkText);
+        }
+
+        private void buttonDownload_Click(object sender, EventArgs e)
+        {
+            browser.FindElement(IconDownload).Click();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            browser.FindElement(IconPlay).Click();
         }
     }
 }
